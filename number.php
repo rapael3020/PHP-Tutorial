ab3266b406e566f263013ec2c42a525a5eb13255
LeeHyunseok <rapael3020@gmail.com>
2020-05-19 22:47:11 +0900

    PHP Tutorial

---
+++ number.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Number&Operator</h1>
+  <h2>1+1</h2>
+<?php
+print(1+1);
+?>
+  <h2>2-1</h2>
+  <?php
+  echo 2-1;//print랑똑같음
+  ?>
+  <h3></h3>
+</body>
+</html>
