ec2558dea42772a37c72b636395e57c206cb2631
LeeHyunseok <rapael3020@gmail.com>
2020-05-24 17:53:16 +0900

    index page adds functions

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<!DOCTYPE html>
-<html>
-    <head>
-      <meta charset="utf-8">
-      <title></title>
-    </head>
-    <body>
-        <h1><a href="index.php">WEB</a></h1>
-        <ol>
-        <?php
-        $list = scandir('data');
+<?php
+function print_title(){
+  if(isset($_GET['id'])){
+    echo $_GET['id'];
+  } else {
+    echo "Welcome Hyunseok's Web";
+  }
+}
+function print_description(){
+  if(isset($_GET['id'])){
+    echo file_get_contents("data/".$_GET['id']);
+    } else {
+      echo "Hello, Hyun's PHP";
+    }
+}
+function print_list(){
+  $list = scandir('data');
         $i = 0;
         while($i<count($list)){
           if($list[$i] != '.'){
@@ -20,23 +26,33 @@
           } 
           $i = $i + 1;
         }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <title>
+      <?php
+        print_title();
+      ?>
+      </title>
+    </head>
+    <body>
+        <h1><a href="index.php">WEB</a></h1>
+        <ol>
+        <?php
+          print_list();
         ?>
         </ol>
         <h2>
-            <?php
-            if(isset($_GET['id'])){
-              echo $_GET['id'];
-            } else {
-              echo "Welcome Hyunseok's Web";
-            }
-            ?>
+        <?php
+            print_title();
+        ?>
         </h2>
         <?php
-        if(isset($_GET['id'])){
-        echo file_get_contents("data/".$_GET['id']);
-        } else {
-          echo "Hello, Hyun's PHP";
-        }
+            print_description();
         ?>
     </body>
 </html>
